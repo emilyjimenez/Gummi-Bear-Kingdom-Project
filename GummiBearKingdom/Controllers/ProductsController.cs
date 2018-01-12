630765ea2989dcf9dac78d912ba377425a6bcdc1
Emily Wells Jimenez <emwellspdx@gmail.com>
2018-01-12 03:03:43 -0800

    building views, running into weird issue having files update after saving them...yellow circle status icon with three dots and cant google what it means.

--- GummiBearKingdom/Controllers/ProductsController.cs
+++ GummiBearKingdom/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using GummiBearKingdom.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,5 +11,12 @@ namespace GummiBearKingdom.Controllers
 {
     public class ProductsController : Controller
     {
+        private GummiBearKingdomDbContext db = new GummiBearKingdomDbContext();
+
+        public IActionResult Index()
+        {
+            List<Product> model = db.Products.ToList();
+            return View(model);
+        }
     }
 }
