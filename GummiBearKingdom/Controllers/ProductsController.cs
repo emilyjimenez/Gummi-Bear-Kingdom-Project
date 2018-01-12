fb8e4f546344c947d05ef50f04c19008a21b5ddb
Emily Wells Jimenez <emwellspdx@gmail.com>
2018-01-12 04:02:02 -0800

    add create product view.

--- GummiBearKingdom/Controllers/ProductsController.cs
+++ GummiBearKingdom/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GummiBearKingdom.Models;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,6 +18,19 @@ namespace GummiBearKingdom.Controllers
         {
             List<Product> model = db.Products.ToList();
             return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
